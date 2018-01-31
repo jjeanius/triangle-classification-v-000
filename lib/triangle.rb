15a6840c50820aaa1308e50b3aae328b75989827
Judy McGoldrick <jjeanius@verizon.net>
2018-01-31 02:02:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,14 +13,19 @@ attr_accessor :a, :b, :c
    def kind
      if @a <= 0 || @b <= 0 || @c <=0
          raise TriangleError
-     elsif @a == @b && @a == @c && @c == @b
+      elsif @a == @b && @a == @c && @c == @b
       :equilateral
-    elsif @a == @b || @a == @c || @b == @c
+      elsif @a == @b || @a == @c || @b == @c
         :isosceles
-    else
+      else
         :scalene
+      end
     end
+
+  if triangle != :equilateral || triangle != :isosceles || triangle != :scalene
+    raise TriangleError
   end
+
 end
 
     class TriangleError < StandardError
