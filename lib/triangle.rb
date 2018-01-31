8fd20d0a4920afc0d85eb206a03c6e85d9cb1aa2
Judy McGoldrick <jjeanius@verizon.net>
2018-01-31 02:17:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,15 +16,13 @@ attr_accessor :a, :b, :c, :triangle
         :equilateral
       elsif @a == @b || @a == @c || @b == @c
         :isosceles
+      elsif triangle != :equilateral || @triangle != :isosceles || @triangle != :scalene
+        raise TriangleError   
       else
         :scalene
       end
-
-    #  @triangle != :equilateral || @triangle != :isosceles || @triangle != :scalene
-    #     raise TriangleError
-       end
-
-    end
+     end
+  end
 
     class TriangleError < StandardError
        def message
